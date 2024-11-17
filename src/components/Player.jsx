76411cce36d122f1260b93afc9f5b34ed2033426
HanaDzin <dzinhana@hotmail.com>
2024-11-17 15:45:29 +0100

    implement useRef to fetch username from input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,33 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  /* using useState:
+    const [name, setName] = useState("");
+    const [submitted, setSubmitted] = useState(false);
+
+    function handleChangeName(e) {
+      setName(e.target.value);
+    }
+
+    function handleClick() {
+      setSubmitted(true);
+    } */
+
+  //using useRef:
+  const [name, setName] = useState("stranger");
+  const playerName = useRef();
+
+  function handleClick() {
+    setName(playerName.current.value);
+    playerName.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome, {name ?? "stranger"}!</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} placeholder="Enter your name" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
